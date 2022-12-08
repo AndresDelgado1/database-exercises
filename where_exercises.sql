202b9db17e7e60bcde8a02eec5590ca7ab4b219a
Andres Delgado <andresdelgado335@gmail.com>
2022-12-08 13:34:34 -0600

    finished where exercises

---
+++ where_exercises.sql
@@ -0,0 +1,47 @@
+USE employees;
+Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN)
+SELECT first_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+
+
+-- Find all emploees whose last name starts with 'E' — 7,330 rows
+SELECT last_name
+FROM employees
+WHERE last_name like 'E%' ;
+
+
+
+-- Find all employees with a 'q' in their last name — 1,873 rows.
+SELECT last_name
+FROM employees
+WHERE last_name like '%q%' ;
+
+
+
+-- Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
+SELECT *
+FROM employees
+WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+
+
+
+-- Find all employees whose last name starts or ends with 'E' — 30,723 rows
+SELECT *
+FROM employees
+WHERE last_name like 'E%' OR last_name like '%E';
+
+
+
+--   Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
+SELECT *
+FROM employees
+WHERE last_name like 'E%' AND last_name like '%E';
+
+
+
+-- Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT *
+FROM employees
+WHERE last_name like '%q%' AND last_name NOT LIKE '%qu%';
