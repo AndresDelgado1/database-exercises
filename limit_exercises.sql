40a66146020b7a57339616e4017b35a348985b2c
Andres Delgado <andresdelgado335@gmail.com>
2022-12-09 11:47:10 -0600

    finished limits

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -5,7 +5,7 @@ SELECT DISTINCT last_name FROM employees ORDER BY last_name DESC LIMIT 10;
 
 
 -- Create a query to get the top 5 salaries and display just the employees number from the salaries table.
--- SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5;
+SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 5;
 SELECT emp_no FROM salaries ORDER BY salary DESC LIMIT 4 OFFSET 46;
 
 
